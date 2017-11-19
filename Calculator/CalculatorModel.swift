03cafdadd11f39f0fc266c49aa9b6ee2c01897cc
CarpDm <skdkfk8758@gmail.com>
2017-11-19 22:26:24 +0900

    First Commit MyCalculator

---
+++ Calculator/CalculatorModel.swift
@@ -0,0 +1,73 @@
+//
+//  CalculatorModel.swift
+//  Calculator
+//
+//  Created by 김동현 on 2017. 11. 18..
+//  Copyright © 2017년 김동현. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorModel {
+    private var accumulator = 0.0
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    private var opeations: Dictionary<String, Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos),
+        "*" : Operation.BinaryOperation({ $0 * $1 }),
+        "/" : Operation.BinaryOperation({ $0 / $1 }),
+        "+" : Operation.BinaryOperation({ $0 + $1 }),
+        "-" : Operation.BinaryOperation({ $0 - $1 }),
+        "=" : Operation.Equals
+    ]
+    
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = opeations[symbol] {
+            switch operation {
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals:
+                executePendingBinaryOperation()
+            }
+        }
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
+
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 김동현 on 2017. 11. 18..
+//  Copyright © 2017년 김동현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentInDisplay = display.text!
+            display!.text = textCurrentInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var model = CalculatorModel()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping{
+            model.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathSymbol = sender.currentTitle {
+            model.performOperation(symbol: mathSymbol)
+        }
+        displayValue = model.result
+    }
+    
+    @IBAction func tappedClearButton(_ sender: Any) {
+        displayValue = 0
+    }
+}
+
